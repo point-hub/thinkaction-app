e61a315a389714ee76b1a6d1d0280cde914bebde
Martien Dermawan Tanama <martiendt@gmail.com>
2026-01-25 09:39:07 +0700

    fix: change the avatar for goal-reminder

--- app/components/menu/notification-panel.vue
+++ app/components/menu/notification-panel.vue
@@ -66,7 +66,7 @@ onMounted(async () => {
         <div class="flex-1 overflow-y-auto p-3 gap-4">
           <template v-for="(notification, index) in notifications?.data" :key="index">
             <div v-if="notification && ['system', 'comment', 'support', 'mention', 'cheers', 'goal-reminder'].includes(notification.type)" class="flex items-start gap-3 p-2 hover:bg-slate-50 transition" :class="{ 'bg-slate-100': !notification.is_read }">
-              <base-avatar v-if="notification.type === 'system'" src="/images/ai.webp" :size="32" />
+              <base-avatar v-if="notification.type === 'system' || notification.type === 'goal-reminder'" src="/images/ai.webp" :size="32" />
               <avatar v-else :size="32" :user="notification.actor" />
 
               <template v-if="notification.type === 'system'">
@@ -129,7 +129,7 @@ onMounted(async () => {
               <template v-else-if="notification.type === 'goal-reminder'">
                 <nuxt-link :to="`/goals/${notification.entities?.goals}`" @click="showSidebar = false">
                   <div class="flex-1 text-sm">
-                    <span class="font-semibold">System</span>
+                    <span class="font-semibold">SYSTEM</span>
                     <span class="text-slate-600 pl-1">You have 1 day left to wrap up your goal.</span>
                     <div class="text-xs text-slate-400">{{ timeAgo(notification.created_at) }}</div>
                   </div>
